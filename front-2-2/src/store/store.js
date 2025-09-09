2a31772d17659b58164f185673a6ba7a5f879e44
ynzung <syj91105@sookmyung.ac.kr>
2025-09-09 20:13:16 +0900

    feat: middleware 적용

--- front-2-2/src/store/store.js
+++ front-2-2/src/store/store.js
@@ -1,24 +1,27 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-const useTodoStore = create((set) => ({
-  todos: [],
-  addTodo: (rawTitle) => {
-    const title = rawTitle.trim();
-    if (!title) return;
-    set((state) => ({
-      todos: [...state.todos, { id: Date.now(), title, completed: false }],
-    }));
-  },
-  removeTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== id),
-    })),
-  toggleTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      ),
-    })),
-}));
+const useTodoStore = create(
+  devtools((set) => ({
+    todos: [],
+    addTodo: (rawTitle) => {
+      const title = rawTitle.trim();
+      if (!title) return;
+      set((state) => ({
+        todos: [...state.todos, { id: Date.now(), title, completed: false }],
+      }));
+    },
+    removeTodo: (id) =>
+      set((state) => ({
+        todos: state.todos.filter((todo) => todo.id !== id),
+      })),
+    toggleTodo: (id) =>
+      set((state) => ({
+        todos: state.todos.map((todo) =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ),
+      })),
+  }))
+);
 
 export default useTodoStore;

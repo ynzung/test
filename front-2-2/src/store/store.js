6f4660cd3c4122e198ca9f419c24ec11bbca3c32
ynzung <syj91105@sookmyung.ac.kr>
2025-09-09 19:34:48 +0900

    feat: store 생성

---
+++ front-2-2/src/store/store.js
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+const useTodoStore = create((set) => ({
+  todos: [],
+  addTodo: (rawTitle) => {
+    const title = rawTitle.trim();
+    if (!title) return;
+    set((state) => ({
+      todos: [...state.todos, { id: Date.now(), title, completed: false }],
+    }));
+  },
+  removeTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id),
+    })),
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    })),
+}));
+
+export default useTodoStore;

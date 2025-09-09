c26674324233d7a6093c737488567a2d5510b752
ynzung <syj91105@sookmyung.ac.kr>
2025-09-09 20:11:17 +0900

    feat: App.jsx 코드 작성

--- front-2-2/src/App.jsx
+++ front-2-2/src/App.jsx
@@ -1,7 +1,55 @@
-import "./App.css";
+import { useState } from "react";
+import TodoItem from "./components/TodoItem";
+import useTodoStore from "./store/store";
+import * as S from "./styles/styled";
 
 function App() {
-  return <></>;
+  const [newTodo, setNewTodo] = useState("");
+  const todos = useTodoStore((s) => s.todos);
+  const addTodos = useTodoStore((s) => s.addTodo);
+
+  const handleAddTodo = () => {
+    if (!newTodo.trim()) return;
+    addTodos(newTodo);
+    setNewTodo("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAddTodo();
+  };
+  return (
+    <>
+      <S.GlobalStyle />
+      <S.Page>
+        <S.Card>
+          <S.H1>투두리스트</S.H1>
+
+          <form onSubmit={handleSubmit}>
+            <S.Row>
+              <S.TextInput
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="할 일을 추가하세요"
+              />
+              <S.PrimaryButton type="submit" title="추가">
+                할일추가
+              </S.PrimaryButton>
+            </S.Row>
+          </form>
+
+          <S.List>
+            {todos.length === 0 ? (
+              <S.Empty>할 일이 없어요. 빨리 추가해봅시다!!!</S.Empty>
+            ) : (
+              todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+            )}
+          </S.List>
+        </S.Card>
+      </S.Page>
+    </>
+  );
 }
 
 export default App;
